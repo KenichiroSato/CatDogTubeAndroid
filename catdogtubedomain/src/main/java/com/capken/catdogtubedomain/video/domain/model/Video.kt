04c16119293006c3dd4db1756844bd1a2543e570
Kenichiro Sato <satok0702@gmail.com>
2017-01-14 14:37:25 +0900

    Remove public

--- catdogtubedomain/src/main/java/com/capken/catdogtubedomain/video/domain/model/Video.kt
+++ catdogtubedomain/src/main/java/com/capken/catdogtubedomain/video/domain/model/Video.kt
@@ -4,11 +4,11 @@ package com.capken.catdogtubedomain.video.domain.model
  * Created by ken on 2017/01/14..
  */
 
-public class Video (val videoId: String,
+class Video (val videoId: String,
                     val title: String,
                     val imageUrl: String) {
 
-    public fun describe() : String {
+    fun describe() : String {
         return videoId + " " + title
     }
 }
