c43f5821aac299c7c01d456f87018bdfdf962242
Kenichiro Sato <satok0702@gmail.com>
2017-01-28 16:35:51 +0900

    added LoadVideoPresenter

--- catdogtubedomain/src/main/java/com/capken/catdogtubedomain/video/domain/model/Video.kt
+++ catdogtubedomain/src/main/java/com/capken/catdogtubedomain/video/domain/model/Video.kt
@@ -1,12 +1,14 @@
 package com.capken.catdogtubedomain.video.domain.model
 
+import java.net.URL
+
 /**
  * Created by ken on 2017/01/14..
  */
 
 class Video (val videoId: String,
              val title: String,
-             val imageUrl: String,
+             val imageUrl: URL,
              val contentType: ContentType) {
 
     fun describe() : String {
