4084541a68f8777375f9b5f0fec423511d1e9c8a
Kenichiro Sato <satok0702@gmail.com>
2017-02-18 12:45:01 +0900

    removed unused code

--- app/src/main/java/com/capken/catdogtube/MainActivity.java
+++ app/src/main/java/com/capken/catdogtube/MainActivity.java
@@ -26,24 +26,16 @@ public final class MainActivity extends AppCompatActivity {
     }
 
     private void setupViews() {
-        PlayerFragment playerFragment = (PlayerFragment)getFragmentManager().findFragmentById(R.id.player_fragment);
+        PlayerFragment playerFragment =
+                (PlayerFragment)getFragmentManager().findFragmentById(R.id.player_fragment);
         PlayVideoPresenter videoPresenter = new PlayVideoPresenter(playerFragment);
 
-        SegmentedFragment segmentedFragment = (SegmentedFragment)getSupportFragmentManager().findFragmentById(R.id.container_segmented);
-        SegmentsPresenter segmentsPresenter = new SegmentsPresenter(segmentedFragment, videoPresenter, new SegmentFactory(this));
+        SegmentedFragment segmentedFragment =
+                (SegmentedFragment)getSupportFragmentManager().findFragmentById(R.id.container_segmented);
+        SegmentsPresenter segmentsPresenter =
+                new SegmentsPresenter(segmentedFragment, videoPresenter, new SegmentFactory(this));
         segmentedFragment.setPresenter(segmentsPresenter);
         segmentsPresenter.loadSegments();
-/*
-        let segmentedVC = UIStoryboard.instantiateVcWithId(SegmentedVC.ID) as! SegmentedVC
-        segmentedVC.view.frame = segmentContainerView.bounds
-        let presenter = SegmentsPresenter(view: segmentedVC as SegmentedContract_View,
-                playerPresenter: playerPresenter,
-                segmentFactory: SegmentFactory())
-        segmentedVC.presenter = presenter
-        segmentContainerView.addSubview(segmentedVC.view)
-        self.addChildViewController(segmentedVC)
-        segmentedVC.didMove(toParentViewController: self)
-        */
 
         Log.d("tag", "setupViews");
     }
