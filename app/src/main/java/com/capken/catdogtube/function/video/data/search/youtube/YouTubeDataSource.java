8d221384166a15a1e1f2ab4f44390213478140ca
Kenichiro Sato <satok0702@gmail.com>
2017-02-27 22:49:11 +0900

    Added publishedBefore param

--- app/src/main/java/com/capken/catdogtube/function/video/data/search/youtube/YouTubeDataSource.java
+++ app/src/main/java/com/capken/catdogtube/function/video/data/search/youtube/YouTubeDataSource.java
@@ -1,6 +1,7 @@
 package com.capken.catdogtube.function.video.data.search.youtube;
 
 import android.util.Log;
+import android.util.TimeUtils;
 
 import com.capken.catdogtubedomain.video.data.YouTubeVideo;
 import com.capken.catdogtubedomain.video.domain.search.SearchVideoDataSourceProtocol;
@@ -10,9 +11,18 @@ import com.google.gson.JsonObject;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
+import info.vividcode.time.iso8601.Iso8601ExtendedOffsetDateTimeFormat;
 import kotlin.Unit;
 import kotlin.jvm.functions.Function1;
 import okhttp3.Call;
@@ -28,10 +38,18 @@ import okhttp3.Response;
 
 public final class YouTubeDataSource implements SearchVideoDataSourceProtocol {
 
+    private final int OLDEST_YEAR = 2011;
+    private final int OLDEST_MONTH = 1;
+    private final int OLDEST_DAY = 1;
+
+
+    private final int SEARCH_PERIOD_DAYS = 30;
+
     @Override
     public void searchVideos(@NotNull String searchWord,
                              @NotNull final Function1<? super List<YouTubeVideo>, Unit> completionHandler) {
 
+        PublishedParamPair publishedParam = generatePublishedParams();
         OkHttpClient client = new OkHttpClient();
         HttpUrl url = new HttpUrl.Builder()
                 .scheme("https")
@@ -44,6 +62,8 @@ public final class YouTubeDataSource implements SearchVideoDataSourceProtocol {
                 .addQueryParameter("videoDuration", "short")
                 .addQueryParameter("maxResults", "30")
                 .addQueryParameter("order", "viewCount")
+                .addQueryParameter("publishedBefore", publishedParam.before)
+                .addQueryParameter("publishedAfter", publishedParam.after)
                 .build();
 
         Request request = new Request.Builder()
@@ -61,6 +81,7 @@ public final class YouTubeDataSource implements SearchVideoDataSourceProtocol {
                 // リクエスト成功時の処理
                 // ステータスコードが200かどうか
                 if (response.code() == 200) {
+                    generatePublishedParams();
                     List<YouTubeVideo> videos = YouTubeDataParser.parse(response.body().string());
                     completionHandler.invoke(videos);
                 } else {
@@ -69,6 +90,50 @@ public final class YouTubeDataSource implements SearchVideoDataSourceProtocol {
                 }
             }
         });
+    }
+
+    private PublishedParamPair generatePublishedParams() {
+
+        Calendar oldestCal = new GregorianCalendar(OLDEST_YEAR, OLDEST_MONTH, OLDEST_DAY);
+        long oldest = oldestCal.getTime().getTime();
+
+        long now = System.currentTimeMillis();
+        long publishedBefore = randomLongBetween(now, oldest);
+        long publishedAfter = publishedBefore - (SEARCH_PERIOD_DAYS * TimeUnit.DAYS.toMillis(1L));
+
+        String beforeStr = RFC3339FromTimeMillis(publishedBefore);
+        String afterStr = RFC3339FromTimeMillis(publishedAfter);
+
+        List<String> params = new ArrayList<>();
+        params.add(beforeStr);
+        params.add(afterStr);
+        Log.d("gap", beforeStr);
+        Log.d("gap", afterStr);
+
+        return new PublishedParamPair(beforeStr, afterStr);
+
+    }
+
+    private long randomLongBetween(long max, long min) {
+        long gap = max - min;
+        Random rand = new Random();
+        long randVal = Math.abs(rand.nextLong() % gap);
+        return min + randVal;
+    }
+
+    private String RFC3339FromTimeMillis(long timeMillis) {
+        DateFormat format = new Iso8601ExtendedOffsetDateTimeFormat();
+        return format.format(timeMillis);
+    }
+
+    class PublishedParamPair {
+        final String before;
+        final String after;
+
+        PublishedParamPair(String before, String after) {
+            this.before = before;
+            this.after = after;
+        }
     }
 
 
