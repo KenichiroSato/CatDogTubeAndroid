d12f50316f262464ba68ab6b8aef28cd3a922acb
Kenichiro Sato <satok0702@gmail.com>
2017-03-04 11:49:00 +0900

    added YouTUbeInfo

---
+++ app/src/main/java/com/capken/catdogtube/function/video/data/search/youtube/YouTubeInfo.java
@@ -0,0 +1,11 @@
+package com.capken.catdogtube.function.video.data.search.youtube;
+
+/**
+ * Created by ken on 2017/02/27..
+ */
+
+public final class YouTubeInfo {
+
+    public static final String INFO = "key";
+
+}
